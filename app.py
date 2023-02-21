8e549467a1ca00c0bbb7098a0133adf4c319e6e1
Jose N Hernandez <nelo.isabal@gmail.com>
2023-02-21 02:59:04 +0000

    working on django app part 1 & 2

--- app.py
+++ app.py
@@ -2,4 +2,4 @@
 #i Please enter the commit message for your changes. Lines starting
 # with '#' will be ignored, and an empty message aborts the commit.
 
-print("Hello World")
+print("Hello Docker World Changed")
